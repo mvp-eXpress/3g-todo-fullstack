9bd0620112fdff8e2ba561bb96c7a0b889640617
Rohan Ray <rohanray@gmail.com>
2019-03-04 15:57:07 +0530

    inter mac

--- core-internal/fe/gqlgen-core/grpc_client/client.go
+++ core-internal/fe/gqlgen-core/grpc_client/client.go
@@ -41,11 +41,29 @@ func (c *Client) GetCollections(ctx context.Context) ([]*gqlgen_core.Collection,
 	}
 	collections := []*gqlgen_core.Collection{}
 	for _, coll := range res.Collections {
+
+		var fvs []*gqlgen_core.FieldValue
+		for _, outfv := range coll.FieldValues {
+			log.Printf("grpc_client::GetCollections::outfv::1 %v", outfv.ValueType)
+			log.Printf("grpc_client::GetCollections::outfv::2 %v", outfv.ValueType.String())
+			log.Printf("grpc_client::GetCollections::outfv::3 %v", v1.TYPE_value[outfv.ValueType.String()])
+			var vt gqlgen_core.ValueType
+			vt = gqlgen_core.ValueType(outfv.ValueType.String())
+			fvs = append(fvs, &gqlgen_core.FieldValue{
+				FieldName: &outfv.FieldName,
+				IsIndexed: &outfv.IsIndexed,
+				IsUnique:  &outfv.IsUnique,
+				ValueType: &vt,
+			})
+		}
+
 		collections = append(collections, &gqlgen_core.Collection{
-			ID:   coll.Id,
-			Host: coll.Host,
-			Name: coll.Name,
+			ID:          coll.Id,
+			Host:        coll.Host,
+			Name:        coll.Name,
+			FieldValues: fvs,
 		})
+
 	}
 	fmt.Printf("grpc_client::GetCollections::Collections: %v", collections)
 	return collections, nil
@@ -54,10 +72,17 @@ func (c *Client) GetCollections(ctx context.Context) ([]*gqlgen_core.Collection,
 func (c *Client) CreateCollection(ctx context.Context, in gqlgen_core.CreateCollectionInput) (*gqlgen_core.Collection, error) {
 	var fvs []*v1.FieldValue
 	for _, inFv := range in.FieldValues {
+		log.Printf("grpc_client::CreateCollection::inFv::1 %v", inFv.ValueType)
+		log.Printf("grpc_client::CreateCollection::inFv::1.1 %v", &inFv.ValueType)
+		log.Printf("grpc_client::CreateCollection::inFv::1.2 %v", *inFv.ValueType)
+		log.Printf("grpc_client::CreateCollection::inFv::2 %v", inFv.ValueType.String())
+		log.Printf("grpc_client::CreateCollection::inFv::3 %v", inFv.ValueType.IsValid())
+
 		fvs = append(fvs, &v1.FieldValue{
 			FieldName: *inFv.FieldName,
 			IsIndexed: *inFv.IsIndexed,
 			IsUnique:  *inFv.IsUnique,
+			ValueType: v1.TYPE(v1.TYPE_value[inFv.ValueType.String()]),
 		})
 	}
 	res, err := c.service.CreateCollection(ctx, &v1.CreateCollectionRequest{
